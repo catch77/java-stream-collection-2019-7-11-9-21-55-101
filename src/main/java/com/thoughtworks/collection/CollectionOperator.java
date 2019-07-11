9794e91ac0cac358609bbd5dbcb08d61733131ee
catch77 <1297056296@qq.com>
2019-07-11 20:32:34 +0800

    finish the Filter part

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -4,28 +4,33 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 
-public class CollectionOperator {
-    public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> getEvenListByIntervals(int left, int right) {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> popEvenElments(int[] array) {
-        throw new NotImplementedException();
-    }
-
-    public int popLastElment(int[] array) {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
-        throw new NotImplementedException();
-    }
-}
+//public class CollectionOperator {
+//    public List<Integer> getListByInterval(int left, int right) {
+//        if (left > right) {
+//            int temp = left;
+//            left = right;
+//            right = temp;
+//        }
+//
+//    }
+//
+//    public List<Integer> getEvenListByIntervals(int left, int right) {
+//        throw new NotImplementedException();
+//    }
+//
+//    public List<Integer> popEvenElments(int[] array) {
+//        throw new NotImplementedException();
+//    }
+//
+//    public int popLastElment(int[] array) {
+//        throw new NotImplementedException();
+//    }
+//
+//    public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
+//        throw new NotImplementedException();
+//    }
+//
+//    public List<Integer> addUncommonElement(Integer[] firstArray, Integer[] secondArray) {
+//        throw new NotImplementedException();
+//    }
+//}
--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -4,6 +4,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -14,18 +15,18 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        return this.array.stream().filter(integer -> integer%2==0).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        return this.array.stream().filter(integer -> integer%3==0).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        return firstList.stream().filter(integer -> secondList.contains(integer)).collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        return this.array.stream().distinct().collect(Collectors.toList());
     }
 }
